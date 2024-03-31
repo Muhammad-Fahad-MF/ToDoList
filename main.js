13b0092d564b5d5cbc1e94ac7a1736b151d35e19
Muhammad-Fahad-MF <imperial.king42@gmail.com>
2024-03-31 06:59:20 +0500

    Some changes to publish

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-console.log("ToDo List");
+//Heading
+console.log("\t\tToDo List");
+//condition for while loop
 let condition = true;
+//assigning array
 let tasks = [];
+//while loop to run program continuously
 while (condition) {
     const todo = await inquirer.prompt([
+        //asking input/todos
         {
             message: "Enter The Tasks",
             name: "item",
             type: "input"
         },
+        //asking for more todos or end program
         {
             message: "Do you want to add more ?",
             name: "askMore",
@@ -17,9 +23,12 @@ while (condition) {
             default: "false"
         }
     ]);
+    //pushing inputs to array
     tasks.push(todo.item);
+    //if condition to end the program
     if (todo.askMore == false) {
         condition = false;
-        console.log(tasks);
     }
 }
+//printing all the tasks/todos
+console.log(tasks);
--- main.ts
+++ main.ts
@@ -2,17 +2,26 @@
 
 import inquirer from "inquirer"
 
-console.log("ToDo List");
+//Heading
+console.log("\t\tToDo List");
+
+//condition for while loop
 let condition = true;
 
+//assigning array
 let tasks = []
+
+//while loop to run program continuously
 while(condition){
 const todo = await inquirer.prompt([
+
+    //asking input/todos
     {
     message : "Enter The Tasks",
     name :"item",
     type:"input"
  },
+ //asking for more todos or end program
  {
     message: "Do you want to add more ?",
     name:"askMore",
@@ -22,12 +31,15 @@ const todo = await inquirer.prompt([
  }
 ])
 
+//pushing inputs to array
 tasks.push(todo.item)
 
+//if condition to end the program
 if(todo.askMore == false){
     condition = false;
 }
 }
 
+//printing all the tasks/todos
 console.log(tasks);
 
