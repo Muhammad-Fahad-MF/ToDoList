f8d4bcd48b3648cea4c2133f80c82948eab6fb55
Muhammad-Fahad-MF <imperial.king42@gmail.com>
2024-03-31 05:14:28 +0500

    created a todo list using while loop

---
+++ main.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+console.log("ToDo List");
+let condition = true;
+
+let tasks = []
+while(condition){
+const todo = await inquirer.prompt([
+    {
+    message : "Enter The Tasks",
+    name :"item",
+    type:"input"
+ },
+ {
+    message: "Do you want to add more ?",
+    name:"askMore",
+    type:"confirm",
+    default:"false"
+
+ }
+])
+
+tasks.push(todo.item)
+
+if(todo.askMore == false){
+    condition = false;
+}
+}
+
+console.log(tasks);
+
